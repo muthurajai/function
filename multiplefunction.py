9a4261147bbf5453a0220a037be8e63495552a42
muthurajai <155083756+muthurajai@users.noreply.github.com>
2024-02-01 11:36:46 +0530

    Add files via upload

---
+++ multiplefunction.py
@@ -0,0 +1,54 @@
+class checkoddeven():
+        def oddeven():
+                num=int(input("enter a number:"))
+                if(num%2)==1:
+                    print(num,end="is odd number")
+                    message="odd number"
+                else:
+                    print(num,end=" is even number")
+                    message="even number"
+                return message
+class sub():
+        def subfields():
+            print("sub_fields in AI are:")
+            list=["mechine learning","vision","neural network","robotics","speech processing","natural language processing"]
+            for ai in list:
+                print(ai)
+class findpercentage():
+        def percentage():
+            a=int(input("subject1="))
+            b=int(input("subject2="))
+            c=int(input("subject3="))
+            d=int(input("subject4="))
+            e=int(input("subject5="))
+            sum=a+b+c+d+e
+            perc=(sum/500)*100
+            print("total=",sum)
+            print(perc)
+class triangle():
+        def triangle():  
+            h=int(input("height:"))
+            b=int(input("breadth:"))
+            areaoftriangle=(h*b)/2
+            print(areaoftriangle)
+        def perimeter():
+            h1=int(input("height1:"))
+            h2=int(input("height2:"))
+            b=int(input("breadth:"))
+            perimeter=h1+h2+b
+            print(perimeter)
+class findpercentage():
+        def percentage():
+            a=int(input("subject1="))
+            b=int(input("subject2="))
+            c=int(input("subject3="))
+            d=int(input("subject4="))
+            e=int(input("subject5="))
+            sum=a+b+c+d+e
+            perc=(sum/500)*100
+            print("total=",sum)
+            print(perc)            
+            
+            
+            
+            
